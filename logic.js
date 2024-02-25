b2f3af49a43e31f7d786f88e9f449f37f9fa9b80
Teboho02 <86244195+Teboho02@users.noreply.github.com>
2024-02-25 00:26:22 +0000

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,1 +1,66 @@
 
+
+const signin = document.getElementById("signin");
+const createAccount = document.getElementById("createAcc");
+let dataReady = false;
+signin.addEventListener("click", function(){
+
+    //console.log("click occured from the independent file");
+
+    if(1==='1'){
+
+        //
+        console.log(document.getElementById("user").textContent);
+        alert("please enter all the required filds");
+
+    }
+    else{
+
+        //create an xml object
+        var req = new XMLHttpRequest();
+        
+        let url = "https://jsonplaceholder.typicode.com/users";
+
+        url.onreadystatechange = function () {
+            console.log(req.state);
+
+            if( req.readyState ==4 && req.status == 200){
+
+            var results = req.responseText;
+            console.log(results);
+            dataReady = true;
+            alert(req);
+
+
+            }
+
+};
+
+
+        req.open("GET", url, true);
+        req.send();
+
+
+
+
+    }
+
+
+});
+
+createAccount.addEventListener("click", function(){
+
+
+});
+
+
+function validate(text){
+    if(text.length < 8 ){
+        return false;
+    }
+
+
+
+}
+
+
